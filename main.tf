afcdfe4131a7c4809a113e3c8c92326c97a1a9a5
Matt Simmons <matt@example.com>
2023-06-30 18:03:34 +0100

    refactor: replace docker compose config with single container (houston-redis)

--- main.tf
+++ main.tf
@@ -47,15 +47,6 @@ resource "google_compute_instance" "vm" {
     scopes = ["cloud-platform"]
   }
 
-  metadata_startup_script = templatefile("${path.module}/startup.sh.tpl",
-  {
-    config_path = "/etc/docker/docker-compose.yaml"
-    houston_password = random_password.admin_password.result
-    tls_host = var.tls_host
-    houston_version = var.houston_version
-    redis_version = var.redis_version
-  })
-
   depends_on = [
     google_project_service.api_setup
   ]
@@ -63,36 +54,37 @@ resource "google_compute_instance" "vm" {
 
 
 // https://registry.terraform.io/modules/terraform-google-modules/container-vm/google/latest
-// this is the equivalent of signing into the VM and running:
-//     docker run -v /etc/docker/docker-compose.yaml:/docker-compose.yaml -v "/var/run/docker.sock:/var/run/docker.sock" docker compose up --wait
+// note: we don't need to say which ports to open
 module "gce-container-houston" {
   source    = "terraform-google-modules/container-vm/google"
   version   = "~> 3.0"
   container = {
-    image        = "docker:latest"
-    command      = ["docker"]
-    args         = ["compose", "-p", "houston", "up", "--wait"]
+    image        = "datasparq/houston-redis:${var.houston_version}"
+    args         = ["api"]
     volumeMounts = [
-      {
-        mountPath = "/docker-compose.yaml"
-        name      = "config"
-        readOnly  = true
-      },
       {
         mountPath = "/var/run/docker.sock"
         name      = "docker_sock"
+      }
+    ]
+    env = [
+      {
+        name = "HOUSTON_PASSWORD"
+        value = random_password.admin_password.result
+      },
+      {
+        name = "HOUSTON_PORT"
+        value = 80
+      },
+      {
+        name = "TLS_HOST"
+        value = var.tls_host
       }
     ]
   }
 
   # Declare the Volumes which will be used for mounting.
   volumes = [
-    {
-      name = "config"
-      hostPath = {
-        path = "/etc/docker/docker-compose.yaml"
-      }
-    },
     {
       name = "docker_sock"
       hostPath = {
@@ -126,33 +118,4 @@ resource "google_compute_address" "public_ip" {
   description = "The static Public IP for the Houston server"
   address_type = "EXTERNAL"
   region = replace(var.zone, "/-.$/", "")  // remove last character from zone to get region
-}
-
-# this forces terraform to wait until the API is ready before continuing
-# this will only work if either curl or wget is installed
-# wget is used because this is available in the official hashicorp/terraform Docker container
-resource "null_resource" "wait-for-availability" {
-  provisioner "local-exec" {
-    command = <<-EOF
-    #!/bin/sh
-    MSG="{\"message\":\"all systems green\"}"
-    healthcheck_1="wget -qO- ${local.base_url}"
-    healthcheck_2="curl ${local.base_url} --silent"
-    count=0
-    while [[ "$($healthcheck_1)" != "$MSG" && "$($healthcheck_2)" != "$MSG" ]] ; do
-      echo "Waiting for Houston API to become available. This can take around 3 minutes."
-      if [[ $count -gt 300 ]] ; then
-        echo -e "Reached the maximum wait time - exiting"
-        exit 1
-      else
-        count=`expr $count + 1`
-        sleep 5
-      fi
-    done
-    EOF
-  }
-  triggers = {
-    always_run = timestamp()
-  }
-  depends_on = [google_compute_instance.vm]
 }
